bc8dae1eaa5d708fa6ce49641e906eeecd864a37
Sabi Ul Hassan <146475150+SABI-UL-HASSAN@users.noreply.github.com>
2025-03-17 03:17:17 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,29 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'dist',
-  },
-  optimizeDeps: {
-    include: ['react-icons/fa'],
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
-  resolve: {
-    alias: {
-      'react': 'preact/compat',
-      'react-dom': 'preact/compat',
-    },
-  },
-
-
+  base: '/laundry/',
 })
